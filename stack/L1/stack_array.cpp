acce892c5c312e0f79544cd0c4296202a172c946
Vaibhav Singh <ihtgoot@gmail.com>
2026-02-08 22:00:01 +0530

    update | 2026-02-08 22:00:01
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   stack/L1/stack_array.cpp
    
     stack/L1/stack_array.cpp | 64 ++++++++++++++++++++++++++++++++++++++++++++++--
     1 file changed, 62 insertions(+), 2 deletions(-)

--- stack/L1/stack_array.cpp
+++ stack/L1/stack_array.cpp
@@ -1,7 +1,67 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-int main(){
+class Stack{
+    private:
+        int *arr;
+        int capacity;
+        int top;
 
+        void grow() {
+            capacity *=2;
+            int * newArr = new int[capacity];
+            for(int i=0;i<top;i++){
+                newArr[i]=arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+
+    public:
+    
+    Stack(int capacity): top(-1), capacity(capacity){
+        arr = new int[capacity];
+    }
+
+    ~Stack(){
+        delete []arr;
+    }
+
+    void push(int value){
+        if(top==capacity){
+            throw overflow_error("Stack Overflow");
+        }
+        else{
+            top++;
+        }
+    }
+
+    void pop(){
+        if(top==-1){
+            throw underflow_error("Stack Underflow");
+        }
+        else{
+            top--;
+        }
+    }
+
+    int size(){
+        return top;
+    }
+
+    int peek(){
+        if(top==-1){
+            throw underflow_error("Stack Underflow");
+        }
+        return arr[top];
+    }
+
+    bool isEmpty(){
+        return top==-1;
+    }
+
+}
+
+int main(){
+    
 }
