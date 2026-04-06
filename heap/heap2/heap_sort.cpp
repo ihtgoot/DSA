3ea401b2a8907e13a05d267a67746f59d8dde053
Vaibhav Singh <ihtgoot@gmail.com>
2026-04-06 21:00:02 +0530

    update | 2026-04-06 21:00:01
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   heap/heap2/heap_sort
    	modified:   heap/heap2/heap_sort.cpp
    
     heap/heap2/heap_sort     | Bin 0 -> 20232 bytes
     heap/heap2/heap_sort.cpp |  29 ++++++++++++++++++++++++++---
     2 files changed, 26 insertions(+), 3 deletions(-)

--- heap/heap2/heap_sort.cpp
+++ heap/heap2/heap_sort.cpp
@@ -1,8 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void heapify(vector<int>&arr,int idx){
-    
+void heapify(vector<int>&arr,int idx, int size){
+    int L = idx;
+    int l = idx*2+1;
+    int r = idx*2+2;
+    if(l<arr.size() && arr[l]<arr[L]){
+        L=l;
+    }
+    if(r<arr.size() && arr[r]<arr[L]){
+        L=r;
+    }
+    if(L!=idx){
+        swap(arr[L],arr[idx]);
+        heapify(arr,L,size);
+    }
+}
+
+void sortHeapSort(vector<int>&arr){
+    int n = arr.size();
+    for(int i=n-1;i>=0;i--){
+        swap(arr[i],arr[0]);
+        heapify(arr,0,i);
+    }
 }
 
 int main(){
@@ -12,5 +32,8 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+    sortHeapSort(arr);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
