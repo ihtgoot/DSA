86c52c9d03af0e76e91e1154a9048f38457ab8b3
Vaibhav Singh <ihtgoot@gmail.com>
2026-04-06 20:30:04 +0530

    update | 2026-04-06 20:30:01
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   heap/heap2/heap_sort.cpp
    	modified:   log.DSA
    
     heap/heap2/heap_sort.cpp | 16 ++++++++++++++++
     log.DSA                  |  4 ++++
     2 files changed, 20 insertions(+)

---
+++ heap/heap2/heap_sort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void heapify(vector<int>&arr,int idx){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n,0);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+}
