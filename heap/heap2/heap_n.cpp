d19cd246267e46767ee8a1d0ce62f8ac1e3bbb12
Vaibhav Singh <ihtgoot@gmail.com>
2026-04-06 19:30:04 +0530

    update | 2026-04-06 19:30:01
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   heap/heap2/heap_n
    	modified:   heap/heap2/heap_n.cpp
    	modified:   log.DSA
    
     heap/heap2/heap_n     | Bin 0 -> 16784 bytes
     heap/heap2/heap_n.cpp |  89 ++++++++++++--------------------------------------
     log.DSA               |  72 ++++++++++++++++++++++++++++++++++++++++
     3 files changed, 93 insertions(+), 68 deletions(-)

--- heap/heap2/heap_n.cpp
+++ heap/heap2/heap_n.cpp
@@ -1,80 +1,33 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-class max_Heap {
-    int *arr;
-    int size;
-    int total_size;
-    
-    void heapify(int idx){
-        int largest = idx;
-        int leftChild = 2*idx+1;
-        int rightChild= 2*idx+2;
-        if(leftChild < size && arr[leftChild]>arr[largest])
-            largest = leftChild;
-        if(rightChild < size && arr[rightChild]>arr[largest])
-            largest = rightChild;
-        if(largest != idx){
-            swap(arr[largest],arr[idx]);
-            heapify(largest);
-        }
+void Heapify(int arr[],int idx,int n){
+    int L = idx;
+    int l = 2*idx+1;
+    int r = 2*idx+2;
+    if(l<n && arr[l]>arr[L]){
+        L = l;
     }
+    if(r<n && arr[r]>arr[L]){
+        L = r;
+    }
+    if(L!=idx){
+        swap(arr[idx],arr[L]);
+        Heapify(arr,L,n);
+    }
+}
 
-    public:
-    
-    max_Heap(int n): size(0), total_size(n){
-        arr = new int[n];
+void BuildMaxHeap(int arr[], int n){
+    for(int i=(n/2)-1;i>=0;i--){
+        Heapify(arr,i,n);
     }
-
-    void insert(int n){
-        if(size==total_size){
-            throw std::overflow_error("heap is full");
-        }
-        arr[size] = n;
-        cout<<"insert_works"<<endl;
-        int idx = size;
-        int parent = (idx-1)/2;
-        while(idx>0 && arr[parent]<arr[idx]){
-            swap(arr[parent],arr[idx]);
-            idx=parent;
-            parent=(idx-1)/2;
-        }
-        size++;
-    }
-
-    int pop(){
-        if(size==0){
-            throw underflow_error("heap underflow");
-        }
-        int top = arr[0];
-        arr[0]=arr[size-1];
-        size--;
-        heapify(0);
-        return top;
-    }
-
-    void print(){
-        int a = 0;
-        for(int i=0;i<size;i++){
-            cout<<arr[i]<<" ";
-            if(a%2==0)
-                cout<<endl;
-            a++;
-        }
-    }
-};
+}
 
 int main(){
-    int n;
-    cin>>n;
-    max_Heap Mheap(n);
-    for(int i=0;i<n;i++){
-        int k;
-        cin>>k;
-        Mheap.insert(k);
+    int arr[] = {10,3,8,9,5,13,18,14,11,70};
+    BuildMaxHeap(arr,10);
+
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
     }
-    Mheap.print();
-    cout<<Mheap.pop()<<endl;
-    Mheap.print();
 }
